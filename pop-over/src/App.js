a3f3d6dc6cb763f4204a06e62555e0d6b0938062
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-07-31 20:21:14 +0530

    the initial code is complete

---
+++ pop-over/src/App.js
@@ -0,0 +1,13 @@
+import Popover from './Components/Popover/Popover';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Popover />
+    </div>
+  );
+}
+
+export default App;
---
+++ pop-over/src/Components/Popover/Popover.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+import "./Popover.css";
+
+const Popover = () => {
+  const [showBody, setShowBody] = useState(false);
+
+  const handleHeaderClick = () => {
+    setShowBody(!showBody);
+  };
+
+  return (
+    <div className="componentContainer">
+      <button onClick={handleHeaderClick}>Click Here</button>
+      {showBody ? (
+        <div className="popoverContainer">
+          <>
+            <div className="popoverHeader">Header</div>
+            <div className="popoverBody">The content is added here</div>
+          </>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Popover;
