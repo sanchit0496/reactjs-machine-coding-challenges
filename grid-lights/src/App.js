a7c51d23d29c607dd286c2a8f84efa14117dc4c6
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-07-31 14:42:54 +0530

    added logic for project here

--- grid-lights/src/App.js
+++ grid-lights/src/App.js
@@ -1,34 +1,65 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [clickedTiles, setClickedTiles] = useState([])
+  const [clickedTiles, setClickedTiles] = useState([]);
+  const [removeTiles, setRemoveTiles] = useState(false);
 
   const handleClick = (i) => {
-    if(i !== 4 && !clickedTiles.includes(i)){
-      setClickedTiles([...clickedTiles, i])
+    if (i !== 4 && !clickedTiles.includes(i)) {
+      setClickedTiles((prev) => {
+        let newTileArray = [...prev, i];
+        console.log("newTileArray", newTileArray);
+        if (newTileArray.length === 8) {
+          setRemoveTiles(true);
+        }
+        return newTileArray;
+      });
     }
-  }
-  console.log('clickedTiles', clickedTiles)
+  };
+  console.log("clickedTiles", clickedTiles);
+
+  useEffect(() => {
+    let interval;
+
+    if (removeTiles) {
+      interval = setInterval(() => {
+        setClickedTiles((prev) => {
+          if (prev.length === 0) {
+            clearInterval(interval);
+            setRemoveTiles(false);
+            return prev;
+          }
+          let newTileArray = prev.slice(0, -1);
+          return newTileArray;
+        });
+      }, 100);
+    }
+    return () => clearInterval(interval);
+  }, [removeTiles]);
 
   const renderTiles = () => {
-    let cells = []
-    for(let i = 0; i < 9; i++){
-        cells.push(
-          <div key = {i} className = {clickedTiles.includes(i) && i !== 4 ? 'tile-filled' :'tile'} onClick = {() => handleClick(i)}>
-            {`Hello ${i}`}
-          </div>
-        )
+    let cells = [];
+    for (let i = 0; i < 9; i++) {
+      cells.push(
+        <div
+          key={i}
+          className={
+            clickedTiles.includes(i) && i !== 4 ? "tile-filled" : "tile"
+          }
+          onClick={() => handleClick(i)}
+        >
+          {`Hello ${i}`}
+        </div>
+      );
     }
-    return cells
-  }
-  
+    return cells;
+  };
+
   return (
     <div className="App">
-      <div className = 'tileHolder'>
-        {renderTiles()}
-      </div>
+      <div className="tileHolder">{renderTiles()}</div>
     </div>
   );
 }
