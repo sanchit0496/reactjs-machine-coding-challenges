a14fbcd9b109699374237d7bc551260af971ade8
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-07-31 12:58:08 +0530

    code complete of like button

--- like-button/src/App.js
+++ like-button/src/App.js
@@ -9,15 +9,39 @@ function App() {
   const [liked, setLiked] = useState(false)
 
 
-  const handleClick = () => {
-    console.log('called')
+  const handleClick = async () => {
     setLiked(!liked)
-    console.log('loading', loading)
+    setLoading(true)
+
+    try {
+      const response = await fetch(
+        "https://www.greatfrontend.com/api/questions/like-button",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            action: liked ? "unlike" : "like",
+          }),
+        },
+      );
+
+      if(response.status === 200){
+        setLiked(!liked)
+      }else{
+        const res = await response.json();
+        setError(res.message);
+        return;
+      }
+    } catch (error) {
+      setError('An unexpected error occured')
+    }finally{
+      setLoading(false)
+    }
   }
   
   return (
     <div className="App">
-      <Button onClick = {handleClick} liked = {liked} loading = {loading} />
+      <Button onClick = {handleClick} liked = {liked} loading = {loading} error = {error} />
     </div>
   );
 }
--- like-button/src/Button/Button.js
+++ like-button/src/Button/Button.js
@@ -2,15 +2,18 @@ import React from "react";
 import { FaHeart, FaFighterJet } from "react-icons/fa";
 import "./Button.css";
 
-const Button = ({ onClick, liked, loading }) => {
+const Button = ({ onClick, liked, loading, error }) => {
 
   return (
+    <div>
     <button className= {liked === true ?  "buttonHolderLiked" : "buttonHolder"} onClick={onClick}>
       <span>
         Button
       </span> 
       {loading === true ? <FaFighterJet /> : <FaHeart/>}
     </button>
+    {error}
+    </div>
   );
 };
 
