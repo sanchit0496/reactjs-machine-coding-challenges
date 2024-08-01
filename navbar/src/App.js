568c45fc03c0d2ec28f8559d7f471d31acaf34ea
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-01 12:45:11 +0530

    have added conditional render of component

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      Hello
     </div>
   );
 }
--- navbar/src/Component/Navbar/Navbar.js
+++ navbar/src/Component/Navbar/Navbar.js
@@ -3,19 +3,19 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const [clickedItemId, setClickedItemId] = useState(0);
-    const [sandwichClicked, setSandwichClicked] = useState(false)
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 460);
+  const [sandwichClicked, setSandwichClicked] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 460);
 
-    const updateIsMobile = () => {
-        setIsMobile(window.innerWidth < 460);
-    };
+  const updateIsMobile = () => {
+    setIsMobile(window.innerWidth < 460);
+  };
 
-    useEffect(() => {
-      window.addEventListener('resize', updateIsMobile)
-      return () => window.removeEventListener('resize', updateIsMobile)
-    }, [])
-    console.log('isMobile', isMobile)
-    
+  useEffect(() => {
+    window.addEventListener("resize", updateIsMobile);
+    return () => window.removeEventListener("resize", updateIsMobile);
+  }, []);
+  console.log("isMobile", isMobile);
+
   const data = [
     { id: "option01", title: "Option 01" },
     { id: "option02", title: "Option 02" },
@@ -29,51 +29,64 @@ const Navbar = () => {
   };
 
   const handleSandwichClick = (e) => {
-    e.stopPropagation()
-    setSandwichClicked(!sandwichClicked)
-  }
+    e.stopPropagation();
+    setSandwichClicked(!sandwichClicked);
+  };
 
   const handleOptionClick = (e, clickedId) => {
-    e.stopPropagation()
+    e.stopPropagation();
     setClickedItemId(clickedId);
-    setSandwichClicked(false)
-  }
-  
+    setSandwichClicked(false);
+  };
+
   const getSandwichOptions = () => {
-    let array = []
+    let array = [];
     data.map((item) => {
-        array.push(
-            <div id = {item.id} className="sandwichOption" onClick={(e) => handleOptionClick(e, item.id)}>{item.title}</div>
-        )
-    })
-    return array
-  }
+      array.push(
+        <div
+          key={item.id}
+          id={item.id}
+          className="sandwichOption"
+          onClick={(e) => handleOptionClick(e, item.id)}
+        >
+          {item.title}
+        </div>
+      );
+    });
+    return array;
+  };
 
   return (
     <div className="navbarContainer">
       <nav>
-        <div className="sandwichContainer" onClick={(e) => handleSandwichClick(e)}>
-          <div className="sandwichLayer"></div>
-          <div className="sandwichLayer"></div>
-          <div className="sandwichLayer"></div>
-          <div className="sandwichOptionContainer">
-          {sandwichClicked && getSandwichOptions()}
-          </div>
-        </div>
-
-        {data.map((item) => (
+        {isMobile ? (
           <div
-            key={item.id}
-            className="navbarOptions"
-            id={item.id}
-            onClick={() => handleClick(item.id)}
-            style={{
-              borderBottom: clickedItemId === item.id ? "5px solid black" : "",
-            }}
+            className="sandwichContainer"
+            onClick={(e) => handleSandwichClick(e)}
           >
-            {item.title}
+            <div className="sandwichLayer"></div>
+            <div className="sandwichLayer"></div>
+            <div className="sandwichLayer"></div>
+            <div className="sandwichOptionContainer">
+              {sandwichClicked && getSandwichOptions()}
+            </div>
           </div>
-        ))}
+        ) : (
+          data.map((item) => (
+            <div
+              key={item.id}
+              className="navbarOptions"
+              id={item.id}
+              onClick={() => handleClick(item.id)}
+              style={{
+                borderBottom:
+                  clickedItemId === item.id ? "5px solid black" : "",
+              }}
+            >
+              {item.title}
+            </div>
+          ))
+        )}
       </nav>
     </div>
   );
