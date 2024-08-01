93cf6c5bda2e1906370bceb642ed80b830a8c131
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-01 11:45:19 +0530

    have added the button

--- navbar/src/Component/Navbar/Navbar.js
+++ navbar/src/Component/Navbar/Navbar.js
@@ -1,37 +1,46 @@
-import React from 'react'
-import { useState } from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [clickedItemId, setClickedItemId] = useState(0);
 
-    const [clickedItemId, setClickedItemId] = useState(0)
+  const data = [
+    { id: "option01", title: "Option 01" },
+    { id: "option02", title: "Option 02" },
+    { id: "option03", title: "Option 03" },
+    { id: "option04", title: "Option 04" },
+    { id: "option05", title: "Option 05" },
+  ];
 
-    const data = [
-        {id: 'option01', title: 'Option 01'},
-        {id: 'option02', title: 'Option 02'},
-        {id: 'option03', title: 'Option 03'},
-        {id: 'option04', title: 'Option 04'},
-        {id: 'option05', title: 'Option 05'}
-    ]
+  const handleClick = (clickedId) => {
+    console.log("clickedId", clickedId);
+    setClickedItemId(clickedId);
+  };
 
-    const handleClick = (clickedId) => {
-        console.log('clickedId', clickedId)
-        setClickedItemId(clickedId)
-    }
-    
   return (
-    <div className='navbarContainer'>
-    <nav>
-        {
-            data.map((item) => {
-                return(
-                    <div className = 'navbarOptions' id= {item.id} onClick = {() => handleClick(item.id)}  style = {{borderBottom : clickedItemId === item.id ?  '5px solid black': ''}}>{item.title}</div>
-                )
-            })
-        }
-    </nav>
+    <div className="navbarContainer">
+      <nav>
+        <div className="sandwichContainer">
+          <div className="sandwichLayer"></div>
+          <div className="sandwichLayer"></div>
+          <div className="sandwichLayer"></div>
+        </div>
+        {data.map((item) => (
+          <div
+            key={item.id}
+            className="navbarOptions"
+            id={item.id}
+            onClick={() => handleClick(item.id)}
+            style={{
+              borderBottom: clickedItemId === item.id ? "5px solid black" : "",
+            }}
+          >
+            {item.title}
+          </div>
+        ))}
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
