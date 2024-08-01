d01a578b014c0e49c6b80bdf2c849c37582f5b43
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-01 09:28:28 +0530

    have added edit feature to app

--- todo-list/src/Components/WorkTile/WorkTile.js
+++ todo-list/src/Components/WorkTile/WorkTile.js
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './WorkTile.css';
 
-const WorkTile = ({title, workList, setWorkList}) => {
+const WorkTile = ({ title, workList, setWorkList }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
 
-    const handleDelete = (clickedTitle) => {
-        let workListFiltered = workList.filter((item) => item !== clickedTitle)
-        setWorkList(workListFiltered)
-    }
+  const handleDelete = (clickedTitle) => {
+    let workListFiltered = workList.filter((item) => item !== clickedTitle);
+    setWorkList(workListFiltered);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    let workListUpdated = workList.map((item) => (item === title ? newTitle : item));
+    setWorkList(workListUpdated);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setNewTitle(title);
+    setIsEditing(false);
+  };
+
+  const handleChange = (event) => {
+    setNewTitle(event.target.value);
+  };
+
   return (
     <div className='tile'>
-      <p className='tileTitle'>{title}</p>
-      <button onClick={() => handleDelete(title)}>Delete</button>
+      {isEditing ? (
+        <>
+          <input
+            type='text'
+            value={newTitle}
+            onChange={handleChange}
+            className='tileInput'
+          />
+          <button onClick={handleSave} className='tileButton saveButton'>Save</button>
+          <button onClick={handleCancel} className='tileButton cancelButton'>Cancel</button>
+        </>
+      ) : (
+        <>
+          <p className='tileTitle'>{title}</p>
+          <button onClick={handleEdit} className='tileButton editButton'>Edit</button>
+          <button onClick={() => handleDelete(title)} className='tileButton deleteButton'>Delete</button>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default WorkTile
+export default WorkTile;
