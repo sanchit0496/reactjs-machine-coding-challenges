31f5a6ab57f058c917bc6d99852d433af7e226a8
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-02 11:49:27 +0530

    completed the otp feature here

--- otp-login/src/App.js
+++ otp-login/src/App.js
@@ -1,49 +1,58 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState, useRef } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const otpArray = [0, 1, 2, 3];
 
-  const otpArray = [0,1,2,3]
-
-  const [activeIndex, setActiveIndex] = useState(0)
-  const [userInput, setUserInput] = useState([])
-  const [displayOTP, setDisplayOTP] = useState(false)
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [userInput, setUserInput] = useState(["", "", "", ""]);
+  const [displayOTP, setDisplayOTP] = useState(false);
+  const inputs = useRef([]);
 
   const handleInputChange = (e, index) => {
-    if(isNaN(Number(e.target.value))){
-      console.log('inside if')
-      return
-    }else{
-      setActiveIndex((prev) => prev + 1)
-      setUserInput((prev) => [...userInput, e.target.value])
+    if (isNaN(Number(e.target.value))) {
+      return;
+    } else if (activeIndex < otpArray.length) {
+      const newUserInput = [...userInput];
+      newUserInput[index] = value;
+      setUserInput(newUserInput);
+      setActiveIndex((prev) => prev + 1);
     }
-  }
+  };
+  console.log("userInput", userInput);
+
+  useEffect(() => {
+    if (activeIndex < otpArray.length) {
+      inputs.current[activeIndex].focus();
+    }
+  }, [activeIndex]);
 
   const handleOTPSubmit = () => {
-    setDisplayOTP(true)
-  }
+    setDisplayOTP(true);
+  };
 
   return (
     <div className="App">
-      OTP Login 
-      <div className = 'otp-holder'>
-        {
-          otpArray.map((item) => {
-            return(
-              <div className = 'otp-digit-holder'>
-                <input 
-                  style={{border: activeIndex === item ? '5px solid pink': ''}} 
-                  disabled = {activeIndex !== item} 
-                  className = {`digit digit-${item}`} 
-                  type = 'text' 
-                  onChange={(e) => handleInputChange(e, item)}  
-                />
-              </div>
-            )
-          })
-        }
+      OTP Login
+      <div className="otp-holder">
+        {otpArray.map((item) => {
+          return (
+            <div className="otp-digit-holder">
+              <input
+                style={{ border: activeIndex === item ? "5px solid pink" : "" }}
+                disabled={activeIndex !== item}
+                className={`digit digit-${item}`}
+                type="text"
+                onChange={(e) => handleInputChange(e, item)}
+                ref={(el) => (inputs.current[item] = el)}
+                value={userInput[item]}
+              />
+            </div>
+          );
+        })}
       </div>
-      <button onClick = {handleOTPSubmit}> Submit </button>
+      <button onClick={handleOTPSubmit}> Submit </button>
       {displayOTP && <div>{`You Entered: ${userInput.join("")}`}</div>}
     </div>
   );
