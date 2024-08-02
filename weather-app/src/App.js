25a8fd5b3bddfb15aeb025de22dd33ff011c5b75
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-02 20:59:50 +0530

    complete css and location data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,26 +7,42 @@ function App() {
   const [dataFetched, setDataFetched] = useState(false)
   const [response, setResponse] = useState(null)
 
-  useEffect(() => {
-    const getWeatherData = () => {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=bf909b4a90c47e810fc156dc73c0ed75"
-      )
-        .then((response) => response.json())
-        .then((result) => {
-          console.log("result", result);
-          setResponse(result)
-          setDataFetched(true)
-        });
-    };
+  const getWeatherData = (latitude,longitude) => {
+    console.log('currentLocation.latitude',latitude, longitude)
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=bf909b4a90c47e810fc156dc73c0ed75`
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("result", result);
+        setResponse(result)
+        setDataFetched(true)
+      });
+  };
 
-    getWeatherData();
+  const showPosition = (pos) => {
+    console.log('poss', pos)
+    getWeatherData(pos.coords.latitude, pos.coords.longitude)
+  }
+
+  useEffect(() => {
+    if(navigator.geolocation){
+      let returnPosition = navigator.geolocation.getCurrentPosition(showPosition)
+    }
   }, []);
+
 
   return (
     <div>
       {dataFetched ? 
-        <div>{response.name}</div> 
+        <>
+          <div className="city-name">{response.name}</div>
+          <div className="description">{`Current Temperature: ${response.main.temp} F`}</div> 
+          <div className="description">{`Description: ${response.weather[0].description} F`}</div> 
+          <div className="description">{`Max Temperature: ${response.main.temp_max} F`}</div> 
+          <div className="description">{`Min Temperature: ${response.main.temp_min} F`}</div> 
+          <div className="description">{`Humidity: ${response.main.humidity} F`}</div> 
+        </>
         : 
         <div>Loading</div>
         }
