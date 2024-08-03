1b1f9b6872f58e648f5e45b59b7cf5f619122c92
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-04 00:14:38 +0530

    the logic of app complete here

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,41 +1,47 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  
-  const[data, setData] = useState([])
-  const [activeButton, setActiveButton] = useState(1)
-  const [activeData, setActiveData] = useState([])
-  const [buttonList, setButtonList] = useState([])
+  const [data, setData] = useState([]);
+  const [activeData, setActiveData] = useState([]);
+  const [buttonList, setButtonList] = useState([]);
 
-  const DATA_TO_DISPLAY = 10
-  const PAGINATION_BUTTONS = data.length/10
-  
+  const DATA_TO_DISPLAY = 10;
 
   useEffect(() => {
-    let array = []
-    for(let i = 0; i < 100; i++){
-      array.push(`Data ${i}`)
+    let array = [];
+    for (let i = 0; i < 100; i++) {
+      array.push(`Data ${i}`);
     }
-    
-    let totalNumberOfButtons = array.length/ DATA_TO_DISPLAY
-    let buttonArray = []
-    for(let i = 0; i < totalNumberOfButtons; i++){
-      buttonArray.push(`Button ${i}`)
+    let tempArray = array.slice(0, DATA_TO_DISPLAY);
+    setActiveData(tempArray);
+
+    let totalNumberOfButtons = array.length / DATA_TO_DISPLAY;
+    let buttonArray = [];
+    for (let i = 0; i < totalNumberOfButtons; i++) {
+      buttonArray.push(i);
     }
-    setData(array)
-    setButtonList(buttonArray)
-  }, [])
-  
+
+    setData(array);
+    setButtonList(buttonArray);
+  }, []);
+
+  const handleClick = (index) => {
+    let tempData = data.slice(
+      index * DATA_TO_DISPLAY,
+      index * DATA_TO_DISPLAY + DATA_TO_DISPLAY
+    );
+    setActiveData(tempData);
+  };
+
   return (
     <div className="App">
-      {
-        buttonList.map((item, index) => {
-          return(
-            <button>{index}</button>
-          )
-        })
-      }      
+      {activeData.map((data) => {
+        return <div>{data}</div>;
+      })}
+      {buttonList.map((item) => {
+        return <button onClick={() => handleClick(item)}>{item}</button>;
+      })}
     </div>
   );
 }
