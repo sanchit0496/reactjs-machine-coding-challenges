b28487a0a0d610f71199493c3ba93458b37a03d4
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-08-25 23:25:24 +0530

    added basic validation of the code

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -4,10 +4,52 @@ import "./App.css";
 function App() {
   const NUMBER_OF_BOXES = 9;
 
+  const winnings = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const userX = [];
+  const userO = [];
+
+  const checkWins = (array) => {
+    let didWin = false;
+    winnings.forEach((item) => {
+      if (
+        item[0] === array[0] &&
+        item[1] === array[1] &&
+        item[2] === array[2]
+      ) {
+        didWin = true;
+      }
+    });
+    return didWin;
+  };
+
+  const handleClick = (i) => {
+    console.log(i);
+    userX.push(i);
+    console.log(userX);
+    if (userX.length >= 3) {
+      let didWin = checkWins(userX);
+      console.log("didWin", didWin);
+    }
+  };
+
   const renderBoard = () => {
     let arr = [];
     for (let i = 0; i < NUMBER_OF_BOXES; i++) {
-      arr.push(<div className = 'box' key={i}>{i}</div>);
+      arr.push(
+        <div onClick={() => handleClick(i)} className="box" key={i}>
+          {i}
+        </div>
+      );
     }
     return arr;
   };
